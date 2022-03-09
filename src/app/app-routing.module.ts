12c434ea1b73c0b6f53689d1d8c643c8a0347626
Eve <eve.arias91@gmail.com>
2022-03-09 15:41:44 -0300

    version 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: TasksComponent},
+  {path:'about', component: AboutComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {enableTracing: true})
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,10 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'hacer tal cosa',
+        day: 'hoy',
+        reminder: false
+    }
+]
